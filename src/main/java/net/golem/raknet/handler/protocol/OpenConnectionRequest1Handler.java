626146a178dfad6cbfcc8bb1cf231833727db78f
Matt <mattjordanbusiness@gmail.com>
2020-06-23 11:05:55 -0500

    whoops...

--- src/main/java/net/golem/raknet/handler/protocol/OpenConnectionRequest1Handler.java
+++ src/main/java/net/golem/raknet/handler/protocol/OpenConnectionRequest1Handler.java
@@ -1,10 +1,12 @@
 package net.golem.raknet.handler.protocol;
 
 import io.netty.channel.ChannelHandlerContext;
+import net.golem.raknet.RakNetConstants;
 import net.golem.raknet.RakNetServer;
 import net.golem.raknet.handler.RakNetInboundPacketHandler;
 import net.golem.raknet.handler.RakNetPacketEnvelope;
 import net.golem.raknet.protocol.DataPacket;
+import net.golem.raknet.protocol.connected.IncompatibleProtocolPacket;
 import net.golem.raknet.protocol.connected.reply.OpenConnectionReply1Packet;
 import net.golem.raknet.protocol.connected.request.OpenConnectionRequest1Packet;
 
@@ -19,9 +21,16 @@ public class OpenConnectionRequest1Handler extends RakNetInboundPacketHandler<Op
 	@Override
 	public DataPacket handlePacket(ChannelHandlerContext context, RakNetPacketEnvelope<OpenConnectionRequest1Packet, SocketAddress> message) {
 		OpenConnectionRequest1Packet packet = message.content();
-		OpenConnectionReply1Packet pk = new OpenConnectionReply1Packet();
-		pk.guid = server.getGlobalUniqueId().getMostSignificantBits();
-		pk.maximumTransferUnits = packet.maximumTransferUnits;
-		return pk;
+		if(packet.networkProtocol != RakNetConstants.PROTOCOL_VERSION) {
+			IncompatibleProtocolPacket pk = new IncompatibleProtocolPacket();
+			pk.protocol = RakNetConstants.PROTOCOL_VERSION;
+			pk.guid = server.getGlobalUniqueId().getMostSignificantBits();
+			return pk;
+		} else {
+			OpenConnectionReply1Packet pk = new OpenConnectionReply1Packet();
+			pk.guid = server.getGlobalUniqueId().getMostSignificantBits();
+			pk.maximumTransferUnits = packet.maximumTransferUnits;
+			return pk;
+		}
 	}
 }
