ca66df7399cb68be2190bdc40d7dc03ba37e0f14
Em Jordan <sylvrs@proton.me>
2022-01-26 17:14:50 -0600

    Move encode to Codable interface

--- src/main/kotlin/raknet/codec/Codable.kt
+++ src/main/kotlin/raknet/codec/Codable.kt
@@ -9,3 +9,23 @@ interface Codable {
     fun decode(buffer: ByteBuf): Any
 
 }
+
+fun Any?.encode(buffer: ByteBuf): ByteBuf {
+    return when (this) {
+        is Byte -> buffer.writeByte(this as Int)
+        is Short -> buffer.writeShort(this as Int)
+        is Int -> buffer.writeInt(this)
+        is Long -> buffer.writeLong(this)
+        is Float -> buffer.writeFloat(this)
+        is Double -> buffer.writeDouble(this)
+        is String -> {
+            buffer.writeShort(this.length)
+            buffer.writeCharSequence(this, Charsets.UTF_8)
+            return buffer
+        }
+        is ByteArray -> buffer.writeBytes(this)
+        is Codable -> this.encode(buffer)
+        // Just return the buffer as is
+        else -> buffer
+    }
+}
