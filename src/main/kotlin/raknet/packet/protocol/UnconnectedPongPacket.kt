3b1bbb6e83d4b1f101385d1c351ddfec69165c6f
Matthew Jordan <mattjordanbusiness@gmail.com>
2022-01-26 17:04:59 -0600

    Implement unconnected packets

---
+++ src/main/kotlin/raknet/packet/protocol/UnconnectedPongPacket.kt
@@ -0,0 +1,35 @@
+package raknet.packet.protocol
+
+import io.netty.buffer.ByteBuf
+import raknet.Magic
+import raknet.packet.DataPacket
+import raknet.packet.PacketType
+import raknet.packet.readMagic
+
+class UnconnectedPongPacket constructor(
+    var pingId: Long,
+    var guid: Long,
+    var magic: Magic = Magic,
+    var serverName: String
+    ): DataPacket(PacketType.UNCONNECTED_PONG.id()) {
+
+    override fun decode(buffer: ByteBuf) {
+
+    }
+
+    companion object {
+        fun from(buffer: ByteBuf): UnconnectedPongPacket {
+            return UnconnectedPongPacket(
+                buffer.readLong(),
+                buffer.readLong(),
+                buffer.readMagic(),
+                buffer.readCharSequence(buffer.readShort().toInt(), Charsets.UTF_8).toString()
+            )
+        }
+    }
+
+    override fun toString(): String {
+        return "UnconnectedPongPacket(pingId=$pingId, guid=$guid, serverName='$serverName')"
+    }
+
+}
