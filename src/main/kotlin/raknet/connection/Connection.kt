f46ede2322be228c6c2eb38a0bd8ba9e0d1ce85d
Em Jordan <sylvrs@proton.me>
2022-01-30 15:52:22 -0600

    Connection: Support sending of connection packets

--- src/main/kotlin/raknet/connection/Connection.kt
+++ src/main/kotlin/raknet/connection/Connection.kt
@@ -2,8 +2,13 @@ package raknet.connection
 
 import io.netty.channel.ChannelHandlerContext
 import io.netty.channel.nio.NioEventLoopGroup
+import raknet.Frame
 import raknet.Server
+import raknet.enums.ReliabilityType
+import raknet.handler.sendPacket
+import raknet.packet.ConnectedPacket
 import raknet.packet.DataPacket
+import raknet.packet.Datagram
 import raknet.packet.protocol.*
 import java.net.InetSocketAddress
 import java.util.concurrent.TimeUnit
@@ -24,6 +29,7 @@ class Connection(
     // TODO: Listeners
     private var worker: NioEventLoopGroup = NioEventLoopGroup()
     private val packetQueue: MutableList<DataPacket> = mutableListOf()
+    private var currentSequenceIndex: Int = 0
 
     init {
         worker.scheduleAtFixedRate(this::tick, 0, TimePeriod.UPDATE.period, TimeUnit.MILLISECONDS)
@@ -50,13 +56,12 @@ class Connection(
         when(packet) {
             is ConnectionRequest -> {
                 state = State.CONNECTING
-
-                send(ConnectionRequestAccepted(
+                sendConnected(ConnectionRequestAccepted(
                     clientAddress = this.address,
                     systemIndex = 0,
                     requestTime = packet.time,
                     time = System.currentTimeMillis(),
-                ), Priority.IMMEDIATE)
+                ))
             }
             is NewIncomingConnection -> {
                 state = State.CONNECTED
@@ -70,18 +75,27 @@ class Connection(
                     pingTime = packet.time,
                     pongTime = System.currentTimeMillis(),
                 )
-                send(response, Priority.IMMEDIATE)
+                sendConnected(response)
             }
             else -> {}
         }
     }
 
     private fun send(packet: DataPacket, priority: Priority = Priority.MEDIUM) {
-        if(priority == Priority.IMMEDIATE) {
-            context.writeAndFlush(packet)
-        } else {
-            packetQueue.add(packet)
-        }
+        TODO("Not implemented")
+    }
+
+    private fun sendConnected(packet: ConnectedPacket, reliability: ReliabilityType = ReliabilityType.UNRELIABLE) {
+        val encoded = packet.prepare()
+        log("Sending connected packet $packet")
+        context.sendPacket(address, Datagram(
+            sequenceIndex = currentSequenceIndex++.toUInt(),
+            frames = mutableListOf(Frame(
+                reliability = reliability,
+                encoded.readableBytes().toUShort(),
+                body = encoded
+            ))
+        ))
     }
 
     private fun close(reason: String) {
