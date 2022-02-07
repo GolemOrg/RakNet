56cebd6f1ed8ebfe8fb0c7f653332de7503b6c26
Em Jordan <sylvrs@proton.me>
2022-02-07 16:01:43 -0600

    IncompatibleProtocol: Fix encoding/decoding + change to offline message

--- src/main/kotlin/raknet/message/protocol/IncompatibleProtocol.kt
+++ src/main/kotlin/raknet/message/protocol/IncompatibleProtocol.kt
@@ -1,18 +1,21 @@
 package raknet.message.protocol
 
 import io.netty.buffer.ByteBuf
-import raknet.message.OnlineMessage
 import raknet.message.MessageType
+import raknet.message.OfflineMessage
+import raknet.types.Magic
+import raknet.types.Magic.readMagic
 
 class IncompatibleProtocol(
-    var protocol: Int,
+    var protocol: Byte,
+    var magic: Magic,
     var serverGuid: Long,
-): OnlineMessage(MessageType.INCOMPATIBLE_PROTOCOL_VERSION.id()) {
+): OfflineMessage(MessageType.INCOMPATIBLE_PROTOCOL_VERSION.id()) {
 
-    override fun encodeOrder(): Array<Any> = arrayOf(protocol, serverGuid)
+    override fun encodeOrder(): Array<Any> = arrayOf(protocol, magic, serverGuid)
 
     companion object {
-        fun from(buffer: ByteBuf) = IncompatibleProtocol(buffer.readInt(), buffer.readLong())
+        fun from(buffer: ByteBuf) = IncompatibleProtocol(buffer.readByte(), buffer.readMagic(), buffer.readLong())
     }
 
     override fun toString() = "IncompatibleProtocol(protocol=$protocol, serverGuid=$serverGuid)"
