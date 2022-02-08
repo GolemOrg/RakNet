c733fe7cc494e1d942720a553de7dba27cd74e59
Matthew Jordan <mattjordanbusiness@gmail.com>
2022-02-07 21:58:51 -0600

    Order: Add ordered encodable interface for encoding

--- src/main/kotlin/raknet/message/datagram/FrameTypes.kt
+++ src/main/kotlin/raknet/message/datagram/FrameTypes.kt
@@ -1,8 +1,11 @@
 package raknet.message.datagram
 
+import raknet.codec.OrderedEncodable
 import raknet.types.UInt24LE
 
-data class Order(val index: UInt24LE, val channel: Byte) {
+data class Order(val index: UInt24LE, val channel: Byte): OrderedEncodable {
+    override fun encodeOrder(): Array<Any> = arrayOf(index, channel)
+
     override fun toString(): String = "Order(index=$index, channel=$channel)"
 }
 
