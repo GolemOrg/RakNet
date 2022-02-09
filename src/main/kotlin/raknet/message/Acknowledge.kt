8161ed627d61b06419d03af4cce47b4f6eb59c4c
Em Jordan <sylvrs@proton.me>
2022-02-09 00:37:07 -0600

    Acknowledge: Implement records as sealed class

--- src/main/kotlin/raknet/message/Acknowledge.kt
+++ src/main/kotlin/raknet/message/Acknowledge.kt
@@ -2,15 +2,35 @@ package raknet.message
 
 import io.netty.buffer.ByteBuf
 import raknet.codec.Decodable
-import raknet.codec.Encodable
-import raknet.types.UIntLE
+import raknet.codec.OrderedEncodable
+import raknet.encode
+import raknet.types.UInt24LE
 
-data class Record(val sequenceNumber: UIntLE, val endSequenceNumber: UIntLE? = null): Encodable, Decodable {
-    override fun encode(buffer: ByteBuf) {}
+sealed class Record(val sequenceNumber: UInt24LE): OrderedEncodable, Decodable {
 
-    override fun decode(buffer: ByteBuf): Any = Unit
+    class Single(sequenceNumber: UInt24LE): Record(sequenceNumber) {
+        override fun getCount(): Int = 1
+        override fun asList(): List<UInt24LE> = listOf(sequenceNumber)
+        override fun encodeOrder(): Array<Any> = arrayOf(sequenceNumber)
+        override fun decode(buffer: ByteBuf): Any = Unit
+        override fun toString(): String = "Record.Single(sequenceNumber=$sequenceNumber)"
+    }
 
-    override fun toString() = "Record(sequenceNumber=$sequenceNumber, endSequenceNumber=$endSequenceNumber)"
+    class Range(sequenceNumber: UInt24LE, val endSequenceNumber: UInt24LE): Record(sequenceNumber) {
+        override fun getCount(): Int = endSequenceNumber.toInt() - sequenceNumber.toInt() + 1
+        override fun asList(): List<UInt24LE> = (sequenceNumber.toInt()..endSequenceNumber.toInt()).map { UInt24LE(it.toUInt()) }
+        override fun encodeOrder(): Array<Any> = arrayOf(sequenceNumber, endSequenceNumber)
+        override fun decode(buffer: ByteBuf): Any = Unit
+        override fun toString(): String = "Record.Range(sequenceNumber=$sequenceNumber, endSequenceNumber=$endSequenceNumber)"
+    }
+
+    abstract fun getCount(): Int
+    abstract fun asList(): List<UInt24LE>
+
+    override fun encode(buffer: ByteBuf) {
+        buffer.writeBoolean(getCount() == 1) // Is Single?
+        encodeOrder().encode(buffer)
+    }
 }
 
 sealed class Base(id: Int, val records: MutableList<Record>): OnlineMessage(id) {
@@ -18,8 +38,35 @@ sealed class Base(id: Int, val records: MutableList<Record>): OnlineMessage(id) 
 }
 
 class Acknowledge(records: MutableList<Record>): Base(MessageType.ACK.id(), records) {
-    override fun toString(): String = "Acknowledge(records=${records.joinToString()})"
+    override fun toString(): String = "Acknowledge(records=$records)"
+
+    companion object  {
+        fun fromQueue(queue: MutableList<UInt>): Acknowledge = Acknowledge(compileRecords(queue))
+    }
 }
 class NAcknowledge(records: MutableList<Record>): Base(MessageType.NACK.id(), records) {
-    override fun toString(): String = "NAcknowledge(records=${records.joinToString()})"
+    override fun toString(): String = "NAcknowledge(records=$records)"
+
+    companion object  {
+        fun fromQueue(queue: MutableList<UInt>): NAcknowledge = NAcknowledge(compileRecords(queue))
+    }
+}
+
+private fun compileRecords(queue: MutableList<UInt>): MutableList<Record> {
+    val records = mutableListOf<Record>()
+    val iterator = queue.iterator()
+    var start = iterator.next()
+    var end = start
+    while(iterator.hasNext()) {
+        val next = iterator.next()
+        if(end - next > 1u) {
+            records.add(if(start != end) Record.Range(UInt24LE(start), UInt24LE(end)) else Record.Single(UInt24LE(start)))
+            start = next
+            end = next
+        } else {
+            end = next
+        }
+    }
+    records.add(if(start != end) Record.Range(UInt24LE(start), UInt24LE(end)) else Record.Single(UInt24LE(start)))
+    return records
 }
