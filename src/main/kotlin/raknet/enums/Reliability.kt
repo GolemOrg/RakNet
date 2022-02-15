65c7ab396d34becee47bb059eaa26a217bd249ed
Matthew Jordan <mattjordanbusiness@gmail.com>
2022-02-15 11:20:35 -0600

    Reliability: Remove unused enum methods

--- src/main/kotlin/raknet/enums/Reliability.kt
+++ src/main/kotlin/raknet/enums/Reliability.kt
@@ -5,11 +5,6 @@ enum class Reliability {
     RELIABLE, RELIABLE_ORDERED, RELIABLE_SEQUENCED,
     UNRELIABLE_WITH_ACK_RECEIPT, RELIABLE_WITH_ACK_RECEIPT, RELIABLE_ORDERED_WITH_ACK_RECEIPT;
 
-    fun reliable(): Boolean = this == RELIABLE || this == RELIABLE_ORDERED || this == RELIABLE_SEQUENCED || this == RELIABLE_WITH_ACK_RECEIPT || this == RELIABLE_ORDERED_WITH_ACK_RECEIPT
-    fun sequenced(): Boolean = this == UNRELIABLE_SEQUENCED || this == RELIABLE_SEQUENCED
-    fun ordered(): Boolean = sequenced() || this == RELIABLE_ORDERED || this == RELIABLE_ORDERED_WITH_ACK_RECEIPT
-    fun ack(): Boolean = this == UNRELIABLE_WITH_ACK_RECEIPT || this == RELIABLE_WITH_ACK_RECEIPT || this == RELIABLE_ORDERED_WITH_ACK_RECEIPT
-
     fun toRaw() = ordinal shl RAW_SHIFT_SIZE
     fun toByte() = ordinal.toByte()
 
