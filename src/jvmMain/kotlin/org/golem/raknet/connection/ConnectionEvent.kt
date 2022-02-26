e5d1978bbc0240a35d7ab4e9776f5002f4b17bde
Matthew Jordan <mattjordanbusiness@gmail.com>
2022-02-26 14:58:55 -0600

    ConnectionEvent: Rename events + add toString() method

--- src/jvmMain/kotlin/org/golem/raknet/connection/ConnectionEvent.kt
+++ src/jvmMain/kotlin/org/golem/raknet/connection/ConnectionEvent.kt
@@ -3,8 +3,16 @@ package org.golem.raknet.connection
 import org.golem.raknet.message.OnlineMessage
 
 sealed class ConnectionEvent {
-    object Connected: ConnectionEvent()
-    class Received(val message: OnlineMessage) : ConnectionEvent()
-    class LatencyUpdated(val latency: Long) : ConnectionEvent()
-    class Disconnected(val reason: DisconnectionReason): ConnectionEvent()
+    object Connected : ConnectionEvent() {
+        override fun toString() = "ConnectedEvent"
+    }
+    class ReceivedMessage(val message: OnlineMessage) : ConnectionEvent() {
+        override fun toString() = "ReceivedMessageEvent(message=$message)"
+    }
+    class LatencyUpdated(val latency: Long) : ConnectionEvent() {
+        override fun toString() = "LatencyUpdatedEvent(latency=$latency)"
+    }
+    class Disconnected(val reason: DisconnectionReason): ConnectionEvent() {
+        override fun toString() = "DisconnectedEvent(reason=$reason)"
+    }
 }
