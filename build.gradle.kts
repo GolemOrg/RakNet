af316a1489cba11b1c0c79fc2c0b96399e36b6b4
Matthew Jordan <mattjordanbusiness@gmail.com>
2022-02-27 02:30:18 -0600

    Exclusively use JITPack

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,32 +26,13 @@ kotlin {
             dependencies {
                 implementation("io.netty:netty-all:4.1.72.Final")
                 implementation("com.esotericsoftware.reflectasm:reflectasm:1.09")
-                implementation("com.github.GolemOrg:events-kt:1.0.2")
+                implementation("com.github.GolemOrg:events:1.0.3")
             }
         }
         val jvmTest by getting {
             dependencies {
                 implementation(kotlin("test"))
-                implementation("com.github.GolemOrg:benchmark-kt:1.0.4")
-            }
-        }
-    }
-}
-
-publishing {
-    publications {
-        create<MavenPublication>("default") {
-            from(components["java"])
-            // Include any other artifacts here, like javadocs
-        }
-    }
-    repositories {
-        maven {
-            name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/GolemOrg/raknet-kt")
-            credentials {
-                username = System.getenv("GITHUB_ACTOR")
-                password = System.getenv("GITHUB_TOKEN")
+                implementation("com.github.GolemOrg:benchpress:1.0.7")
             }
         }
     }
