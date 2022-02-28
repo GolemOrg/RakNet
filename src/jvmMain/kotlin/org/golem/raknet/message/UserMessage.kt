43f4c3f39354e393fcb65445b1a6c93ff758f370
Matthew Jordan <mattjordanbusiness@gmail.com>
2022-02-28 03:40:24 -0600

    UserMessage: Fix incorrect encoding

--- src/jvmMain/kotlin/org/golem/raknet/message/UserMessage.kt
+++ src/jvmMain/kotlin/org/golem/raknet/message/UserMessage.kt
@@ -3,7 +3,7 @@ package org.golem.raknet.message
 import io.netty.buffer.ByteBuf
 
 class UserMessage(id: Int, val buffer: ByteBuf): OnlineMessage(id) {
-    override fun encodeOrder(): Array<Any> = arrayOf(id, buffer)
+    override fun encodeOrder(): Array<Any> = arrayOf(buffer)
     override fun toString(): String = "UserMessage(id=$id, buffer=ByteBuf(${buffer.readableBytes()}))"
 
 }
