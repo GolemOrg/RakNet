e0a7bbde9cd3920f0eaff5484c2196fd48c6c46d
Matthew Jordan <mattjordanbusiness@gmail.com>
2022-03-03 23:03:32 -0600

    Connection: Decouple state & time periods from file

--- src/jvmMain/kotlin/org/golem/raknet/connection/Connection.kt
+++ src/jvmMain/kotlin/org/golem/raknet/connection/Connection.kt
@@ -13,16 +13,6 @@ import java.util.concurrent.TimeUnit
 const val MIN_MTU: Short = 576
 const val MAX_MTU: Short = 1492
 
-enum class ConnectionState { INITIALIZING, CONNECTING, CONNECTED, DISCONNECTED }
-
-enum class TimeComponent(private val millis: Long) {
-    UPDATE(10),
-    PING(5_000),
-    TIMEOUT(30_000);
-
-    fun toLong(): Long = millis
-}
-
 class Connection(
     val address: InetSocketAddress,
     val server: Server,
@@ -90,10 +80,7 @@ class Connection(
 
     fun send(packet: OnlineMessage, immediate: Boolean = false) = internalsHandler.send(packet, immediate)
 
-    fun ping() {
-        if(!isConnected) return
-        send(ConnectedPing(time = server.getUptime()), true)
-    }
+    fun ping() = send(ConnectedPing(time = server.getUptime()), true)
 
     fun close(reason: DisconnectionReason) {
         worker.shutdownGracefully()
