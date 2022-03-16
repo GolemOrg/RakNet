7af6dce221d42a054c2c7da8fb392abb60e79d00
Em Jordan <sylvrs@proton.me>
2022-03-16 17:28:19 -0500

    Datagram: Better handle internal frames and automatically release after sending

--- src/jvmMain/kotlin/org/golem/raknet/handler/MessageEnvelope.kt
+++ src/jvmMain/kotlin/org/golem/raknet/handler/MessageEnvelope.kt
@@ -7,4 +7,11 @@ import java.net.InetSocketAddress
 class MessageEnvelope<T: DataMessage>(
     packet: T,
     address: InetSocketAddress
-): DefaultAddressedEnvelope<T, InetSocketAddress>(packet, null, address)
+): DefaultAddressedEnvelope<T, InetSocketAddress>(packet, null, address) {
+
+    /**
+     * Given that the error usually returned by `super.release()` is a IllegalReferenceCountException,
+     * we can safely ignore it and default to true.
+     */
+    override fun release(): Boolean = runCatching { super.release() }.getOrDefault(true)
+}
